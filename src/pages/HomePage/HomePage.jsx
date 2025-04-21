eb6592a4bf881cae17c186725bfba4c871d6bcb3
balo-max <shoni6124@gmail.com>
2025-04-21 19:13:43 +0200

    upd styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,16 +7,18 @@ export default function HomePage() {
     const isLoggedIn = useSelector(selectIsLoggedIn);
 
     return (
-        <div className={css.homeWrapper}>
-            <div className={css.secondWrapper}>
-                <h1 className={css.title}>Welcome to 'Your Contacts Book'.</h1>
-                <p className={css.titleText}>With us your contacts will be safe and organized. Create, edit, use!</p>
-                {!isLoggedIn ? <div className={css.linkWrapper}>
-                    <p className={css.textReg}>Ready to get started? </p>
-                    <p className={css.linkText}><NavLink className={css.link} to='/registration'>Register</NavLink> to create an account. <br /> Already registered? <NavLink className={css.link} to='/login'>Login</NavLink>.</p>
-                </div> : <NavLink className={css.linkStart} to='/contacts'>Start</NavLink>}
-                
-            </div>
+        <div className={css.wrapper}>
+            <div className={css.homeWrapper}>
+                <div className={css.secondWrapper}>
+                    <h1 className={css.title}>Welcome to 'Your Contacts Book'.</h1>
+                    <p className={css.titleText}>With us your contacts will be safe and organized. Create, edit, use!</p>
+                    {!isLoggedIn ? <div className={css.linkWrapper}>
+                        <p className={css.textReg}>Ready to get started? </p>
+                        <p className={css.linkText}><NavLink className={css.link} to='/registration'>Register</NavLink> to create an account. <br /> Already registered? <NavLink className={css.link} to='/login'>Login</NavLink>.</p>
+                    </div> : <NavLink className={css.linkStart} to='/contacts'>Start</NavLink>}
+            
+                </div>
+        </div>
         </div>
     )
 }
